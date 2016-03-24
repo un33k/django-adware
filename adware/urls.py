014cdae3fd9ae03d89a36801d33e4edff4d06cf5
Val Neekman <val@neekware.com>
2016-03-24 18:35:01 -0400

    add urls

--- adware/urls.py
+++ adware/urls.py
@@ -1,1 +1,17 @@
-urls.py
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls import patterns
+
+from .views import *
+from . import defaults as defs
+
+
+urlpatterns = [
+
+    url(
+        r'^update$',
+        AdSenseView.as_view(),
+        name='adsense_update'
+    ),
+
+]
